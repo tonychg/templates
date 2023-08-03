4a9afe272b635049f13191659c3e5dcb45b74dcc
tonychg <tonychg7@pm.me>
2023-08-03 17:24:26 +0200

    feat(templates): update templating scripts

--- python/template.sh
+++ python/template.sh
@@ -2,5 +2,9 @@
 
 projectname=$1
 
-sed -i -e "s#projectname#${projectname}#g"
+sed -i -e "s#projectname#${projectname}#g" Taskfile.yml
+sed -i -e "s#projectname#${projectname}#g" pyproject.toml
+sed -i -e "s#projectname#${projectname}#g" README.md
+sed -i -e "s#projectname#${projectname}#g" flake.nix
 mv projectname $projectname
+rm -f template.sh
