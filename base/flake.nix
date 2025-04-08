fe24f8ead07f7f98406c11501cdd72e987e6639b
tonychg <tonychg7@pm.me>
2025-04-08 14:19:21 +0200

    feat(base): add missing envrc

--- base/flake.nix
+++ base/flake.nix
@@ -1,16 +1,7 @@
 {
-  inputs = {
-    utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-        ];
-      };
-    }
-  );
+  inputs = { utils.url = "github:numtide/flake-utils"; };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { devShell = pkgs.mkShell { buildInputs = with pkgs; [ ]; }; });
 }
