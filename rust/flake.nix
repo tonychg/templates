11efea71b85b8a6b35ce4b4872a9d2a2771179d9
tonychg <tonychg7@pm.me>
2025-07-17 22:36:04 +0200

    style: format flake

--- rust/flake.nix
+++ rust/flake.nix
@@ -10,30 +10,34 @@
       };
     };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
 
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          # compile-time deps
-          packages = with pkgs; [
-            curl
-            wget
-            pkg-config
-            rustToolchain
-          ];
-        in
-        with pkgs;
-        {
+        # compile-time deps
+        packages = with pkgs; [
+          curl
+          wget
+          pkg-config
+          rustToolchain
+        ];
+      in
+        with pkgs; {
           devShells.default = mkShell {
             nativeBuildInputs = packages;
           };
         }
-      );
+    );
 }
